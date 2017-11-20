70bbf59ff3353820b84f72a28c21edc096582cb6
Jinsu Oh <heyjinsu@gmail.com>
2017-11-20 10:21:00 -0800

    Fix layouts plugin contents issue

--- build.js
+++ build.js
@@ -7,39 +7,41 @@ var argv = require('minimist')(process.argv);
 // If I run node run deploy --prod, it should not use browser-sync to watch for changes.
 // Otherwise, it should.
 if (!argv.deploy) {
-    browserSync({
-        port: 8000,
-        server: 'build',
-        files: ['src/**/*'],
-        middleware: function (req, res, next) {
-            build(next);
-        }
-    })
+  browserSync({
+    port: 8000,
+    server: 'build',
+    files: ['src/**/*'],
+    middleware: function (req, res, next) {
+      build(next);
+    }
+  })
 } else {
-    build(function () {
-        console.log('Done building.');
-    })
+  build(function () {
+    console.log('Done building.');
+  })
 }
 
 function build(callback) {
   Metalsmith(__dirname)
-    .metadata({
-      title: "My Static Site & Blog",
-      description: "It's about saying »Hello« to the World.",
-      generator: "Metalsmith",
-      url: "http://www.metalsmith.io/"
-    })
+  .metadata({
+    title: "My Static Site & Blog",
+    description: "It's about saying »Hello« to the World.",
+    generator: "Metalsmith",
+    url: "http://www.metalsmith.io/"
+  })
   .source('./src/pages')
-    .destination('./build')
-    .clean(false)
-    .use(assets({
-      source: './src/assets',
-      destination: './assets'
-    }))
-  .use(layouts({
-    engine: 'handlebars'
+  .destination('./build')
+  .clean(true)
+  .use(assets({
+    source: './src/assets',
+    destination: './assets'
   }))
-
+  .use(layouts({
+    engine: 'handlebars',
+    directory: "./src/layouts",
+    partials: "./src/partials",
+    pattern: ["**/*.html", "*html"],
+  }))
   .build(function (err, files) {
     if (err) { throw err; }
     var message = 'Build complete';
